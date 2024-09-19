40eae73f1e6723ccb6b988ad7f3f7a77e095584b
Camily <cavicfina@gmail.com>
2024-09-19 17:13:00 -0300

    Curso de Python

---
+++ aula1.py
@@ -0,0 +1,9 @@
+#Comentários são feitas assim com o #
+"""
+é uma DocString, o interpretador lê
+esse é um jeito de escrever como se fosse um comentário só que multilinha
+podendo pular várias linhas
+"""
+
+#função para imprimir algo na tela, toda função tem () que recebem argumentos
+print(123)
